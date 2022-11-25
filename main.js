ce89479a4c03c76ac83796e5ed8b3932a86b6051
lichaunlin <lichaunlin@gmail.com>
2022-11-25 12:43:32 +0800

    Sync

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const { exec } = require('node:child_process');
+const { execSync } = require('node:child_process');
 const path = require("path");
 const { Command } = require('commander');
 
 
 var program = new Command();
 program
-    .version(require('../package.json').version)
+    .version(require('./package.json').version)
     .option('-i, --input <type>', 'location of  the  swagger  spec,  as  URL  or file', 'https://petstore.swagger.io/v2/swagger.json')
     .option('-o, --output <type>', 'where to write  the  generated  files(current dir by default)', 'services')
     .option('-t, --template <type>', 'folder containing the template files', 'template/typescript-axios-custom')
@@ -63,39 +63,25 @@ console.log({
 
 var cmd = `java -jar ${JarFilePath} generate -i ${inputPath} -t "${templatePath}" --lang ${lang} -o "${outputPath}"`;
 
-console.log(cmd);
+try {
+    console.log();
+    console.log(cmd);
 
-if (true) {
-    exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
+    var msg = execSync(cmd).toString();
 
-        if (stdout) {
-            console.log(`stdout: ${stdout}`);
-        }
+    console.log();
+    console.log(msg);
+ } catch (error) {
+    console.log("status: ", error.status);  // 0 : successful exit, but here in exception it has to be greater than 0
+    console.log("message: ", error.message); // Holds the message you typically want.
+    console.log("stderr: ", error.stderr);  // Holds the stderr output. Use `.toString()`.
+    console.log("stdout: ", error.stdout);  // Holds the stdout output. Use `.toString()`.
+ }
 
-        if (stderr) {
-            console.error(`stderr: ${stderr}`);
-        }
-    });
+var packetFilePath = path.join(outputPath, "package.json");
+if (fs.existsSync(packetFilePath)) {
+    console.log('delete ', packetFilePath);
 
-    var packetFilePath = path.join(outputPath, "package.json");
-    console.log('delete package.json, ', packetFilePath);
-
-    fs.rm(packetFilePath, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-
-        if (stdout) {
-            console.log(`stdout: ${stdout}`);
-        }
-
-        if (stderr) {
-            console.error(`stderr: ${stderr}`);
-        }
-    });
+    fs.rmSync(packetFilePath);
 }
+
